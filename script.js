fac83e5eb0f6f18ee77be32bd5c8c0091c78bc95
Giovanna Sousa <giovannasousa54@gmail.com>
2022-10-18 16:35:54 -0300

    Último requisito

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const botaoEnviar = document.getElementById('submit-btn');
 const checar = document.getElementById('agreement');
 const contador = document.getElementById('counter');
 const texto = document.getElementById('textarea');
+// const main = document.getElementsByTagName('main');
+// const form = document.getElementById('evaluation-form');
 
 function login() {
   if (email.value === 'tryber@teste.com' && senha.value === '123456') {
@@ -34,3 +36,42 @@ function contandoCaracteres() {
 }
 
 texto.addEventListener('keyup', contandoCaracteres);
+
+let learn, family, house, rate;
+
+const learnCheck = [];
+const eraseForm = document.getElementById('evaluation-form');
+const firstName = document.getElementById('input-name');
+const lastName = document.getElementById('input-lastname');
+const userEmail = document.getElementById('input-email');
+
+function checkSelection() {
+  family = document.querySelector('input[name=family]:checked').value;
+  learn = document.getElementsByClassName('subject');
+  rate = document.querySelector('input[name=rate]:checked').value;
+  house = document.getElementById('house').value;
+
+  for (let i = 0; i < learn.length; i += 1) {
+    if (learn[i].checked) {
+      learnCheck.push(` ${learn[i].value}`);
+    }
+  }
+}
+
+botaoEnviar.addEventListener('click', () => {
+  checkSelection();
+  eraseForm.style.display = 'none';
+  const formulario = document.createElement('form');
+  const formInfos = document.querySelector('main');
+  formInfos.appendChild(formulario);
+  formulario.setAttribute('id', 'form-data');
+  const newForm = document.getElementById('form-data');
+  newForm.innerHTML = `Nome: ${firstName.value} ${lastName.value}`;
+  newForm.innerHTML += `<br> Email: ${userEmail.value}`;
+  newForm.innerHTML += `<br> Casa: ${house}`;
+  newForm.innerHTML += `<br> Família: ${family}`;
+  newForm.innerHTML += `<br> Matérias:${learnCheck}`;
+  newForm.innerHTML += `<br> Avaliação: ${rate}`;
+  newForm.innerHTML += `<br> Observações: ${texto.value}`;
+  event.preventDefault();
+});
